2329c3f638c1bc2cb52b170a758960225b416d22
JohanAbrahamssonHis <118519456+JohanAbrahamssonHis@users.noreply.github.com>
2025-01-16 00:48:58 +0100

    Sprites of mines

--- MineSweeperRougeLikeProject/Assets/Scripts/Mine.cs
+++ MineSweeperRougeLikeProject/Assets/Scripts/Mine.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Mine
+public abstract class Mine : MonoBehaviour
 {
     public int weight;
+    public Sprite sprite;
     
     // Start is called before the first frame update
     void Start()
