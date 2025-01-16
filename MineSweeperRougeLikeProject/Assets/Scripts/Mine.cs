14e8d44b292e60544b4e0a4c5a3e9fe51159c0de
JohanAbrahamssonHis <118519456+JohanAbrahamssonHis@users.noreply.github.com>
2025-01-17 00:22:28 +0100

    Scriptable objects added

--- MineSweeperRougeLikeProject/Assets/Scripts/Mine.cs
+++ MineSweeperRougeLikeProject/Assets/Scripts/Mine.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public abstract class Mine : MonoBehaviour
 {
     public int weight;
     public Sprite sprite;
+    public Vector2 position;
+    public delegate void CallBack(string hello);
+
+    public CallBack call;
     
     // Start is called before the first frame update
-    void Start()
+    public void SetUpMine()
+    {
+        SpriteRenderer spriteRenderer = this.AddComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprite;
+    }
+
+    public void SetPostion(Vector2 pos)
+    {
+        position = pos;
+    }
+
+    public void Suckda(string hello)
     {
         
     }
@@ -16,6 +32,12 @@ public abstract class Mine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        call += Suckda;
+        call += Suckda;
+
         
+        
+        call("hello");
+
     }
 }
