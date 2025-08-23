5cf30c0af0901cd4b0655a2f5baeb8334d6abf38
JohanAbrahamssonHis <118519456+JohanAbrahamssonHis@users.noreply.github.com>
2025-08-23 13:33:56 +0200

    TitleWiggle

--- MineSweeperRougeLikeProject/Assets/TitleWiggle.cs
+++ MineSweeperRougeLikeProject/Assets/TitleWiggle.cs
@@ -6,14 +6,19 @@ using UnityEngine;
 public class TitleWiggle : MonoBehaviour
 {
     private List<GameObject> _gameObjects;
+    private List<Vector3> _positions;
     public float speed;
+    public float strength;
+    public float delay;
     void Start()
     {
         _gameObjects = new List<GameObject>();
+        _positions = new List<Vector3>();
         
         foreach (Transform c in GetComponentInChildren<Transform>())
         {
             _gameObjects.Add(c.gameObject);
+            _positions.Add(c.position);
         }
 
     }
@@ -21,6 +26,9 @@ public class TitleWiggle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _gameObjects.ForEach(x => x.transform.localPosition += new Vector3(0,MathF.Sin(speed*Time.deltaTime),0));
+        for (int i = 0; i < _gameObjects.Count; i++)
+        {
+            _gameObjects[i].transform.localPosition = _positions[i] + new Vector3(0, MathF.Sin(speed * (Time.time-i*delay))*strength, 0);
+        }
     }
 }
