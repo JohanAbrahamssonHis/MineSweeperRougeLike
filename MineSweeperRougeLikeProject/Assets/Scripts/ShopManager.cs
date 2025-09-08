43c94b5e5fc151c0bca5ed001f36727405c1a547
JohanAbrahamssonHis <118519456+JohanAbrahamssonHis@users.noreply.github.com>
2025-09-08 23:50:45 +0200

    Comments in Shop manger

--- MineSweeperRougeLikeProject/Assets/Scripts/ShopManager.cs
+++ MineSweeperRougeLikeProject/Assets/Scripts/ShopManager.cs
@@ -42,21 +42,28 @@ public class ShopManager : MonoBehaviour
 
     private List<Item> GetRarityValue()
     {
+        //Total Odds of all rarities
         int totalOdds = 0;
         for (int i = 1; i <= HighestRarity; i++) totalOdds += i;
         
+        //Gets a random number
         int rarityValueSelected = Random.Range(1, totalOdds+1);
         
+        //Gets a test value (currentValue) and ses if it is over that value
         int rarityValue = 0;
-        int temp = 0;
+        int currentValue = 0;
         for (int i = 1; i <= HighestRarity; i++)
         {
-            temp += i;
-            if (rarityValueSelected > temp) continue;
+            //Adds current rarity
+            currentValue += i;
+            //If it is not over, then go to next rarity
+            if (rarityValueSelected > currentValue) continue;
+            //Set rarity value to the inverse of what i is (Higher i means less rare item)
             rarityValue = HighestRarity+1-i;
             break;
         }
         
+        //returns the list of what items have that rarity and have not been banned to showup
         return ItemList.Where(x => x.rarity == rarityValue && !BannedItemList.Contains(x)).ToList();
     }
 }
